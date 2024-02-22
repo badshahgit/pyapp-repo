6e1b5417501ef3437583f499e7bfaac8f9d29bf9
sultan321 <122960863+badshahgit@users.noreply.github.com>
2024-02-22 12:42:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,12 +7,12 @@ app = Flask(__name__)
 # Route to the root URL
 @app.route('/')
 def hello():
-    return 'Hello, Flask on Docker!'
+    return 'Welcome to ELAI AGRI!'
 
 # Route to a custom endpoint
 @app.route('/greet/<name>')
 def greet(name):
-    return f'Hello, {name}! Welcome to Flask on Docker.'
+    return f'Hello, {name}! Welcome to ELAI AGRI!.'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3000)
